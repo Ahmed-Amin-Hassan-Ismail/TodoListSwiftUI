8dfa5a553c94d5d41d7df05e60949939745bfc32
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-02 23:32:14 +0200

    initial commit

---
+++ TodoListSwiftUI/TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by Ahmed Amin on 02/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
